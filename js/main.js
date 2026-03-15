032f8c440cfe5797fe7d37cf9c6717873188dd8f
Debanjan-2007 <debanjanmukherjee2007@gmail.com>
2026-03-16 01:34:09 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loading-screen');
   if (loader) {
-    setTimeout(() => loader.classList.add('hidden'),50);
+    setTimeout(() => loader.classList.add('hidden'),100);
   }
 });
 
